daa9b21d99b74249631754d3dac0117c6df2d57b
nguyenthieuluan <trangkhuyet30@gmail.com>
2018-07-04 19:25:46 +0700

    4/7 lbb

--- index.php
+++ index.php
@@ -1,24 +1,42 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<link rel="stylesheet" type="text/css" href="css/content.css" />
+<link rel="stylesheet" type="text/css" href="css/footer.css" />
+<link rel="stylesheet" type="text/css" href="css/menu.css" />
+
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Website  Bán Ấm Điện</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Nguyễn Thiệu Luân</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
 
 </head>
 <body>
-	<div class="wrapper">
-   
-    	<?php
-			include('admincp/modules/config.php');
-			include('modules/header.php');
-			include('modules/menu.php');
-			include('modules/content.php');
-			include('modules/footer.php');
-		?>
-     <script type="text/javascript" src="js/jquery-3.2.1.js"></script>
-	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-    <script type="text/javascript" src="js/tabs.js"></script>
-      
-        </div>
+	<nav>
+		<?php include('modules/menu.html'); ?>
+	</nav>
+	<header>
+		<?php include('modules/header.html'); ?>
+	</header>
+
+	<div class="container">
+		<div class="wrapper">
+	    	<?php
+				include('admincp/modules/config.php');
+				include('modules/content.html');
+			?>
+		</div>
+	</div>
+
+	<footer>
+		<?php include('modules/footer.html'); ?>
+	</footer>
 </body>
 </html>
